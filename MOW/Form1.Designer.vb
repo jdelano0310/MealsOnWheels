37d856fc25eb8e64cd3e3a99f629275d9b3be862
Jon <jd31068@hotmail.com>
2023-10-11 08:46:57 -0400

    Add project files.

---
+++ MOW/Form1.Designer.vb
@@ -0,0 +1,39 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmMDI
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        SuspendLayout()
+        ' 
+        ' frmMDI
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(0), CByte(56), CByte(81))
+        ClientSize = New Size(934, 501)
+        IsMdiContainer = True
+        Name = "frmMDI"
+        Text = "Meals On Wheels"
+        ResumeLayout(False)
+    End Sub
+
+End Class
---
+++ MOW/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class frmMDI
+    Private Sub frmMDI_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' set the background color of the main form
+        Dim myColor As Color = System.Drawing.ColorTranslator.FromHtml("#003851")
+
+        For Each ctrl As Control In Me.Controls
+            If TypeOf ctrl Is MdiClient Then
+                ctrl.BackColor = myColor
+                Exit For
+            End If
+        Next
+    End Sub
+End Class
